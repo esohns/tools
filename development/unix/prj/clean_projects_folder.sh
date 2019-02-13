c9db22b79161e3463b5ac005682b76ae8c6b9e02
Erik Sohns <esohns@localhost.localdomain>
2019-02-13 16:25:58 +0100

    minor changes

--- development/unix/prj/clean_projects_folder.sh
+++ development/unix/prj/clean_projects_folder.sh
@@ -6,9 +6,14 @@
 # return value: - 0 success, 1 failure
 
 # sanity checks
+command -v basename >/dev/null 2>&1 || { echo "basename is not installed, aborting" >&2; exit 1; }
+command -v cd >/dev/null 2>&1 || { echo "cd is not installed, aborting" >&2; exit 1; }
 command -v dirname >/dev/null 2>&1 || { echo "dirname is not installed, aborting" >&2; exit 1; }
+command -v find >/dev/null 2>&1 || { echo "find is not installed, aborting" >&2; exit 1; }
 command -v git >/dev/null 2>&1 || { echo "git is not installed, aborting" >&2; exit 1; }
 command -v realpath >/dev/null 2>&1 || { echo "realpath is not installed, aborting" >&2; exit 1; }
+command -v rm >/dev/null 2>&1 || { echo "rm is not installed, aborting" >&2; exit 1; }
+command -v xargs >/dev/null 2>&1 || { echo "xargs is not installed, aborting" >&2; exit 1; }
 
 DEFAULT_PROJECTS_ROOT_DIRECTORY="$(dirname $(realpath -e $0))/../../../.."
 PROJECTS_DIRECTORY=${DEFAULT_PROJECTS_ROOT_DIRECTORY}
@@ -28,17 +33,14 @@ for PROJECT_DIRECTORY in ${SUB_DIRECTORIES}
 do
  echo "\"${PROJECT_DIRECTORY}\"..."
 
- CURRENT_PROJECT_DIRECTORY="${PROJECTS_DIRECTORY}/${PROJECT_DIRECTORY}/cmake/cotire"
+ CURRENT_PROJECT_DIRECTORY="${PROJECTS_DIRECTORY}/${PROJECT_DIRECTORY}/cmake"
  if [[ ! -d ${CURRENT_PROJECT_DIRECTORY} ]]; then
  echo "\"${CURRENT_PROJECT_DIRECTORY}\"..."
   continue
  fi
-
- echo "\"${CURRENT_PROJECT_DIRECTORY}\"..."
-
- rm -rf  ${CURRENT_PROJECT_DIRECTORY}
+ cd ${CURRENT_PROJECT_DIRECTORY}
+ find . -name cotire | xargs rm -rf
  [ $? -ne 0 ] && echo "ERROR: failed to remove \"${CURRENT_PROJECT_DIRECTORY}\": $?, aborting" && exit 1
- echo "removed \"${CURRENT_PROJECT_DIRECTORY}\""
 
  echo "\"$PROJECT_DIRECTORY\"...DONE"
 done
